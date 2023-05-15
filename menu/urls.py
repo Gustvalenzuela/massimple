376a7cafa5bd4f983bb89fad8ddf70fe8de1f382
Gustvalenzuela <107148810+Gustvalenzuela@users.noreply.github.com>
2023-05-15 15:43:45 -0400

    productos
    imagenes

--- menu/urls.py
+++ menu/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import principal,detergentes
+from .views import principal,detergentes,productos
 
 urlpatterns = [
     path('',principal,name="inicio"),
     path('paginadetergentes',detergentes,name="detergentes"),
+    path('paginadeproductos',productos,name="productos"),
+
 ]
