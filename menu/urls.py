3c4a9c3a2326e12503b3edeb6df29213fa05d730
valecc11 <vale.castroc@duocuc.cl>
2023-05-16 01:47:24 -0400

    vistas y urls

--- menu/urls.py
+++ menu/urls.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import principal,detergentes,productos
+from .views import principal,detergentes,productos, proteccion,login,cambiocontr,Carrito,crearcuenta,EditarPerfil,Herramientas,listado,Otros,perfiladmin,perfilusuario,recuperar,DlavalozaLim
 
 urlpatterns = [
     path('',principal,name="inicio"),
     path('paginadetergentes',detergentes,name="detergentes"),
     path('productosAseo',productos, name="productos"),
+    path('paginaproteccion',proteccion,name="proteccion"),
+    path('paginalogin',login,name="login"),
+    path('cambiocontr',cambiocontr,name="cambiocontr"),
+    path('Carrito', Carrito,name="Carrito"),
+    path('crearcuenta',crearcuenta,name="crearcuenta"),
+    path('EditarPerfil',EditarPerfil,name="EditarPerfil"),
+    path('Herramientas',Herramientas,name="Herramientas"),
+    path('listado',listado,name="listado"),
+    path('Otros',Otros,name="Otros"),
+    path('perfiladmin',perfiladmin,name="perfiladmin"),
+    path('perfilusuario',perfilusuario,name="perfilusuario"),
+    path('recuperar',recuperar,name="recuperar"),
+    path('DlavalozaLim',DlavalozaLim, name="DlavalozaLim",)
+    
+
+    
+    
     
     
 ]
--- menu/views.py
+++ menu/views.py
@@ -17,4 +17,42 @@ def proteccion(request):
 def login(request):
     return render(request, 'menu/login.html')
 
+def cambiocontr(request):
+    return render(request, 'menu/cambiocontr.html')
+
+def Carrito(request):
+    return render(request, 'menu/Carrito.html')
+
+def crearcuenta(request):
+    return render(request, 'menu/crearcuenta.html')
+
+def EditarPerfil(request):
+    return render(request, 'menu/EditarPerfil.html')
+
+def Herramientas(request):
+    return render(request, 'menu/Herramientas.html')
+
+def listado(request):
+    return render(request, 'menu/listado.html')
+
+def Otros(request):
+    return render(request, 'menu/Otros.html')
+
+def perfiladmin(request):
+    return render(request, 'menu/perfiladmin')
+
+def perfilusuario(request):
+    return render(request, 'menu/perfilusuario')
+
+def recuperar(request):
+    return render(request, 'menu/recuperar.html')
+
+def DetalleMasimple(request):
+    return render(request, 'menu/DetalleMasimple.html')
+
+def DlavalozaLim(request):
+    return render(request,'menu/D.lavalozaLim.html')
+
+
+
 
