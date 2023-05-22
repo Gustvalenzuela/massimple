4aec7a8993a8f92bd9e3edc6ae384fcc4e5884f0
Gustvalenzuela <107148810+Gustvalenzuela@users.noreply.github.com>
2023-05-22 16:06:34 -0400

    TABLA
    los

--- menu/models.py
+++ menu/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-class rol(models.Model):
+class Rol(models.Model):
     idRol = models.AutoField(primari_key = True, verbose_name='Id del Rol')
     nombreRol= models.CharField(max_length=30, verbose_name = 'Nombre del Rol', null=True, blank=False)
 
 
-class pregunta(models.Model):
+class Pregunta(models.Model):
     idPregunta = models.AutoField(primari_key = True, verbose_name='id de la Pregunta')
     nombrePregunta = models.CharField(max_length=30, verbose_name='Nombre de la Pregunta', null=True, blank,False)
 
-class categoria(models.Model):
+class Categoria(models.Model):
     idCategoria = models.IntergerField(primari_key = True, verbose_name= 'Id de la categoria')
     nombreCategoria = models.CharField(max_length=30, verbose_name='Nombre de las categoria', null= True, blank=False)
 
 
 
-class usuario(models.Model):
+class Usuario(models.Model):
     idUsuario = models.IntergerField(primary_key=True)
     rutUsuario = models.IntergerField()
     nombreUsuario = models.CharField(max_length=30)
@@ -27,4 +27,5 @@ class usuario(models.Model):
     claveUsuario= models.CharField(max_length=35)
     direccionUsuario= models.CharField(max_legth= 50)
     respuestaUsuario= models.ChardField(max_length= 100)
-    Pregunta= models.ForeignKey(Pregunta,)
+    pregunta= models.ForeignKey(Pregunta,on_delete=models.CASCADE)
+    rol = models.ForeignKey(Rol,on_delete=models.CASCADE)
