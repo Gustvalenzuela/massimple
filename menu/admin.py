52f516bb2dbe54d1563450d75d3e6cd75cee3fee
valecc11 <vale.castroc@duocuc.cl>
2023-05-24 01:23:36 -0400

    importar models

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Rol, Pregunta, Categoria, Usuario, Producto, Compra, Detalle
 
 # Register your models here.
+admin.site.register(Rol)
+admin.site.register(Pregunta)
+admin.site.register(Categoria)
+admin.site.register(Usuario)
+admin.site.register(Producto)
+admin.site.register(Compra)
+admin.site.register(Detalle)
